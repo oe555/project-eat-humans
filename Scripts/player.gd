b44813286c1c0cdc95e09698c77a3969592b544d
Jesse Carnaxide <jcarnaxide@gmail.com>
2026-04-25 00:49:33 -0400

    controls feel more spacey, accelerate, break, etc

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,15 +2,27 @@ extends CharacterBody2D
 
 
 @export var rotation_speed: float = PI
-@export var thrust_speed: float = 150.0
+@export var thrust_acceleration: float = 100.0
+@export var thrust_max: float = 150.0
+@export var thrust_decay: float = 10.0
 
 
 func _physics_process(delta: float) -> void:
 
+	## Calculate spin of the ship
 	var rotation_direction = Input.get_axis("rotate_counterclockwise", "rotate_clockwise")
 	rotation += delta * rotation_speed * rotation_direction
 
-	var thrust = Input.get_axis("reverse", "accelerate")
-	velocity = thrust * thrust_speed * Vector2.DOWN.rotated(rotation)
+	if Input.is_action_pressed("break"):
+		velocity = velocity.move_toward(Vector2.ZERO, thrust_acceleration * delta)
+	elif Input.is_action_pressed("accelerate"):
+		velocity += thrust_acceleration * delta * Vector2.DOWN.rotated(rotation)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, thrust_decay * delta)
+
+	## Make sure ship doesn't exceed its maximum speed
+	velocity = velocity.limit_length(thrust_max)
+
+	print(velocity.length())
 
 	move_and_slide()
