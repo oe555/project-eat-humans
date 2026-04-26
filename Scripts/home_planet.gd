8c1446c40f98a120d6f6e1ec06be05c6c5769641
anthologen <anthologen@gmail.com>
2026-04-26 02:28:18 -0400

    add days counter to UI

--- Scripts/home_planet.gd
+++ Scripts/home_planet.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		print("Player entered Home Planet!")
+		GameState.days_passed += 1
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -6,6 +6,7 @@ extends CanvasLayer
 @onready var fuel_label: Label = $Gauges/FuelGauge/FuelLabel
 @onready var humans_label: Label = $Gauges/HumansLabel
 @onready var gold_label: Label = $Gauges/GoldLabel
+@onready var day_label: Label = $DayLabel
 
 var player: CharacterBody2D
 
@@ -25,3 +26,4 @@ func _process(_delta: float) -> void:
 
 		humans_label.text = "HUMANS %d/%d" % [player.humans, player.human_capacity]
 		gold_label.text = "GOLD %d" % GameState.gold
+		day_label.text = "DAY %d" % GameState.days_passed
