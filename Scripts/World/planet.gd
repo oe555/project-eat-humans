dfed5b77bab6f4e0be0a2b05087916658c8b3ab8
oe555 <oweneckart@outlook.com>
2026-04-25 22:56:52 -0400

    Fixed thruster color

--- Scripts/World/planet.gd
+++ Scripts/World/planet.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 var data: PlanetData
 
-@onready var _sprite: Sprite2D = $Sprite2D
+#@onready var _sprite: Sprite2D = $Sprite2D
 @onready var _collision: CollisionShape2D = $CollisionShape2D
 
 func setup(d: PlanetData) -> void:
@@ -20,14 +20,14 @@ func setup(d: PlanetData) -> void:
 			shape.radius = 50
 		_collision.shape = shape
 
-	if _sprite and _sprite.texture:
-		var tex_size := _sprite.texture.get_size()
-		if tex_size.x > 0.0:
-			var radius: float = 0.0
-			if d.planet_type == 1:
-				radius = 40.0
-			if d.planet_type == 2:
-				radius = 35.0
-			if d.planet_type == 3:
-				radius = 50.0
-			#_sprite.scale = Vector2.ONE * (radius * 2.0 / tex_size.x)
+	# if _sprite and _sprite.texture:
+	# 	var tex_size := _sprite.texture.get_size()
+	# 	if tex_size.x > 0.0:
+	# 		var radius: float = 0.0
+	# 		if d.planet_type == 1:
+	# 			radius = 40.0
+	# 		if d.planet_type == 2:
+	# 			radius = 35.0
+	# 		if d.planet_type == 3:
+	# 			radius = 50.0
+	# 		#_sprite.scale = Vector2.ONE * (radius * 2.0 / tex_size.x)
